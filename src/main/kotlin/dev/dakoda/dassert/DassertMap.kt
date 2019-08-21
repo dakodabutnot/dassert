5a024985cf59c17d2b2b44e9c22c1b99d50329bc
dakodabutnot <southforwinter6@gmail.com>
2019-08-21 08:42:08 +0100

    updated syntax for map key/value assertions

--- src/main/kotlin/dev/dakoda/dassert/DassertMap.kt
+++ src/main/kotlin/dev/dakoda/dassert/DassertMap.kt
@@ -1,58 +1,30 @@
 package dev.dakoda.dassert
 
-import dev.dakoda.dassert.MapDassert.MapDassertDSL.ValExpect
-import dev.dakoda.dassert.MapDassert.MapDassertDSL.ValExpect.Companion.ValExpectant
 import org.opentest4j.AssertionFailedError
-import java.lang.RuntimeException
+import java.util.ArrayList
 
-fun <K, V> dassertMap(block: MutableMap<K, ValExpect<K, V>>.() -> Unit): ValExpectant<K, V> = ValExpectant(block)
-
-fun <K, V> MutableMap<K, ValExpect<K, V>>.that(valExpect: ValExpect<K, V>) {
-    this[valExpect.key] = valExpect
+fun <K, V> Dassert.map(map: MutableMap<K, V>, block: MapDassertion<K, V>.() -> Unit) {
+    block.invoke(MapDassertion(map))
 }
 
-infix fun <K, V> Any.maps(value: V): ValExpect<K, V> {
-    return ValExpect(this as K, value)
-}
+class MapDassertion<K, V>(
+    private val mutableMap: MutableMap<K, V>
+) {
 
-class MapDassert {
+    private var mapDassert = mutableMapOf(mutableMap to arrayListOf<MapDassertExpectant<K, V>>())
 
-    class MapDassertDSL {
-
-        class ValExpect <K, V> (val key: K, val value: V) {
-
-            companion object {
-
-                private fun <K, V> MutableMap<K, ValExpect<K, V>>.assertAsEqualTo(otherMap: Map<K, V>) {
-                    for (valExpect in this.values) {
-                        val key = valExpect.key
-                        val value1 = otherMap[key]
-                        val value2 = this@assertAsEqualTo[key]?.value
-                        try {
-                            dassert {
-                                value1 equals value2
-                            }
-                        } catch (assertionFailure: AssertionFailedError) {
-                            if (value1 == null) {
-                                throw RuntimeException("Error during assertion. The key [$key] does not exist in the supplied map.")
-                            } else {
-                                throw RuntimeException("Error during assertion. At key [$key], expected value " +
-                                        "to be ${assertionFailure.expected} but was ${assertionFailure.actual}.")
-                            }
-                        }
-                    }
-                }
-
-                class ValExpectant <K, V>(
-                    private val block: MutableMap<K, ValExpect<K, V>>.() -> Unit
-                ) {
-                    fun on(otherMap: MutableMap<K, V>) {
-                        mutableMapOf<K, ValExpect<K, V>>().also(block).assertAsEqualTo(otherMap)
-                    }
-                }
+    @Suppress("UNCHECKED_CAST")
+    infix fun <K: Any, V> K.maps(value: V) {
+        try {
+            dassert {
+                (mutableMap as MutableMap<K, V>) has this@maps
+                mutableMap[this@maps] equals value
             }
-
+            (mapDassert[mutableMap]!! as ArrayList<MapDassertExpectant<K, V>>).add(MapDassertExpectant(this, value))
+        } catch (e: AssertionFailedError) {
+            throw AssertionFailedError("Assertion failed. Expecting key [${this}] to map onto ${e.expected}, but was ${e.actual}.")
         }
-
     }
+
+    class MapDassertExpectant<K, V>(val key: K, val value: V)
 }
